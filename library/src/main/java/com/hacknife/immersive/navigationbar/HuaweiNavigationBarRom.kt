da195936523d434e11ae4d927808c167bd3064d5
hacknife <hacknife@outlook.com>
2021-01-11 15:19:37 +0800

    HuaweiNavigationBarRom.kt

--- library/src/main/java/com/hacknife/immersive/navigationbar/HuaweiNavigationBarRom.kt
+++ library/src/main/java/com/hacknife/immersive/navigationbar/HuaweiNavigationBarRom.kt
@@ -5,19 +5,27 @@ import android.os.Build
 import android.provider.Settings
 import android.util.Log
 import androidx.annotation.RequiresApi
-import androidx.fragment.app.FragmentActivity
+import com.hacknife.immersive.NavigationBarRom
+import com.hacknife.immersive.TAG
 
 /**
  * author : 段泽全(hacknife)
  * e-mail : hacknife@outlook.com
  * time   : 2019/8/5
- * desc   : MVVM
+ * desc   : 华为
  * version: 1.0
  */
-class EmuiNavigationBarRom : NavigationBarRom {
-    @RequiresApi(Build.VERSION_CODES.KITKAT)
-    override fun navigationBarExist(activity: Activity): Boolean {
-        return Settings.Global.getInt(activity.contentResolver, "navigationbar_is_min", 0) == 0
+class HuaweiNavigationBarRom : NavigationBarRom {
+    companion object {
+        const val CONTENT_KEY = "navigationbar_is_min"
     }
 
+    init {
+        Log.v(TAG, "HuaweiNavigationBarRom")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    override fun navigationBarExist(activity: Activity): Boolean {
+        return Settings.Global.getInt(activity.contentResolver, CONTENT_KEY, 0) == 0
+    }
 }
