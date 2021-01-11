677070f28d80a158d80161de6af3811009ee2264
hacknife <hacknife@outlook.com>
2021-01-11 15:21:25 +0800

    Vivo

---
+++ library/src/main/java/com/hacknife/immersive/navigationbar/VivoNavigationBarRom.kt
@@ -0,0 +1,32 @@
+package com.hacknife.immersive.navigationbar
+
+import android.app.Activity
+import android.os.Build
+import android.provider.Settings
+import android.util.Log
+import androidx.annotation.RequiresApi
+import com.hacknife.immersive.NavigationBarRom
+import com.hacknife.immersive.TAG
+
+/**
+ * author : hacknife
+ * e-mail : hacknife@outlook.com
+ * time   : 2019/8/5
+ * desc   : vivo
+ * version: 1.0
+ */
+class VivoNavigationBarRom : NavigationBarRom {
+    companion object {
+        const val CONTENT_KEY = "navigation_gesture_on"
+    }
+
+    init {
+        Log.v(TAG, "VivoNavigationBarRom")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    override fun navigationBarExist(activity: Activity): Boolean {
+        return Settings.Secure.getInt(activity.contentResolver, CONTENT_KEY, 0) == 0
+    }
+
+}
